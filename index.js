b511a6bdd9b80329ccb7918d759985ba3e368893
luc <1devnote1@gmail.com>
2025-05-14 02:05:25 -0600

    middleware

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Router } = require("express")
+const { query, validationResult } = require("express-validator")
+const bodyParser = require("body-parser");
+
+const middleware = Router();
+
+middleware.use(bodyParser.json());
+middleware.use(bodyParser.urlencoded())
+
+
+const validator = (req, res, next) => {
+  
+  next()
+}
+
+module.exports = middleware
---
+++ test.js
@@ -0,0 +1,5 @@
+const { Router } = require("express")
+const { query, validationResult } = require("express-validator")
+
+const router = Router();
+
